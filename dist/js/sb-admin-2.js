f7cf13e35958fbe0448eb16eb41b2fd0f2586f7b
jnowacki <jedrzejenowacki@gmail.com>
2017-01-22 21:37:25 +0100

    fixed: page height

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -48,3 +48,24 @@ $(function () {
         }
     }
 });
+
+$(document).ready(function () {
+    var topOffset = 50;
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (width < 768) {
+        $('div.navbar-collapse').addClass('collapse');
+        topOffset = 100; // 2-row-menu
+    } else {
+        $('div.navbar-collapse').removeClass('collapse');
+    }
+
+    var height = ((window.innerHeight > 0) ? window.innerHeight : screen.height) - 1;
+
+
+
+    height = height - topOffset;
+    if (height < 1) height = 1;
+    if (height > topOffset) {
+        $("#page-wrapper").css("min-height", (height) + "px");
+    }
+});
